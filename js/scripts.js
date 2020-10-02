ab0cca9c0f6525e4ca4b4b50fdebc5dfaff820bb
Nina Kin <matikin9@gmail.com>
2020-10-02 16:59:51 -0700

    retrieve data, show data based on selection

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,27 @@
 const data_url = "https://spreadsheets.google.com/feeds/list/15oORluAXFWZBD1thTLHIZADg2fBNhJ2LrcLCDXftcpM/od6/public/values?alt=json";
+let allBusChanges = {};
 
 $.getJSON(data_url , function(data) {
-  var entry = data.feed.entry;
-  $(entry).each(function() {
-    console.log(this.gsx$oldlinenumber.$t);
+  allBusChanges = data.feed.entry;
+
+  $(document).ready(function() {
+    $(allBusChanges).each(function() {
+      $('#oldBusLines').append('<option>' + this.gsx$oldlinenumber.$t + '</option>');
+    });
+  });
+});
+
+$('#submitBusQuery').click(function() {
+  let myBusLine = $('#myBusLine').val();
+  $(allBusChanges).each(function(index, element) {
+    if (element.gsx$oldlinenumber.$t == myBusLine) {
+      $('#busChanges').html(
+        '<span>Line Changes: ' + element.gsx$linenumberchange.$t + '</span><br>' + 
+        '<span>Route Changes: ' + element.gsx$routechanges.$t + '</span><br>' + 
+        '<span>Stop Changes: ' + element.gsx$stopchanges.$t + '</span><br>' + 
+        '<span>Frequency Changes: ' + element.gsx$frequencychanges.$t + '</span>'
+      );
+      return false;
+    }
   });
 });
