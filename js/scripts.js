04f8b1de1f3f931daceabf1e6d2f38bf9dfb8fad
Nina Kin <matikin9@gmail.com>
2020-10-02 15:37:58 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
+const data_url = "https://spreadsheets.google.com/feeds/list/15oORluAXFWZBD1thTLHIZADg2fBNhJ2LrcLCDXftcpM/od6/public/values?alt=json";
 
+$.getJSON(data_url , function(data) {
+  var entry = data.feed.entry;
+  $(entry).each(function() {
+    console.log(this.gsx$oldlinenumber.$t);
+  });
+});
