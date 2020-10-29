e1d85470cad3de1995fa9b10a240b9dd1b4b0ba6
Nina Kin <matikin9@gmail.com>
2020-10-29 14:21:29 -0700

    auto-suggest, all bus lines, and bus description

--- js/scripts.js
+++ js/scripts.js
@@ -9,28 +9,34 @@ const NEXTGEN_CHANGES = {
 
 let allBusChanges = {};
 
-$.getJSON(DATA_URL , function(data) {
+$.getJSON(DATA_URL, function (data) {
   allBusChanges = data.feed.entry;
 
-  $(document).ready(function() {
-    $(allBusChanges).each(function() {
-      $('#myBusLine').append('<option>' + this.gsx$linenumber.$t + '</option>');
+  $(document).ready(function () {
+    $(allBusChanges).each(function () {
+      $('#busLines').append('<option>' + this.gsx$linenumber.$t + '</option>');
     });
   });
 });
 
-$('#myBusLine').change(function() {
+$('#myBusLine').change(function () {
   let myBusLine = $('#myBusLine').val();
   let myChanges = "";
 
-  $(allBusChanges).each(function(index, element) {
+  $(allBusChanges).each(function (index, element) {
     if (element.gsx$linenumber.$t == myBusLine) {
-      myChanges = "<h2>Changes affecting your line:</h2>";
-      myChanges += "<div>" + getChanges(element) + "</div>"
-      myChanges += "<a href=\"" + element.gsx$url.$t + "\">View the new timetable for details.</a>";
+      myChanges = "<h2>Line " + myBusLine + " - " + element.gsx$linedescription.$t + "</h2>";
 
+      if (element.gsx$linechanges.$t == 'no') {
+        myChanges += "<h3>No changes to your line right now</h3>";
+        myChanges += "<div>Sign up here if you'd like to be emailed about changes to your line </div>";
+      } else {
+        myChanges += "<h3>Changes affecting your line:</h3>";
+        myChanges += "<div>" + getChanges(element) + "</div>"
+        myChanges += "<a href=\"" + element.gsx$url.$t + "\">View the new timetable for details.</a>";
+      }
       $('#busChanges').html(
-        myChanges 
+        myChanges
       );
       return false;
     }
@@ -49,15 +55,15 @@ function getChanges(data) {
     }
 
     if (data.gsx$linererouted.$t == "yes") {
-      returnValues += "<span>" + NEXTGEN_CHANGES.line_rerouted + "</span>";      
+      returnValues += "<span>" + NEXTGEN_CHANGES.line_rerouted + "</span>";
     }
 
     if (data.gsx$stopschanged.$t == "yes") {
-      returnValues += "<span>" + NEXTGEN_CHANGES.stops_changed + "</span>";      
+      returnValues += "<span>" + NEXTGEN_CHANGES.stops_changed + "</span>";
     }
 
     if (data.gsx$owldiscontinued.$t == "yes") {
-      returnValues += "<span>" + NEXTGEN_CHANGES.owl_discontinued + "</span>";      
+      returnValues += "<span>" + NEXTGEN_CHANGES.owl_discontinued + "</span>";
     }
   }
 
