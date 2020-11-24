9e2488f1937d0e343add7321cb9b8d5e8d6d5325
Nina Kin <matikin9@gmail.com>
2020-11-24 12:01:57 -0800

    add details about Transit app preview mode for Dec changes

--- js/scripts.js
+++ js/scripts.js
@@ -24,7 +24,13 @@ function getData(url) {
 
 AJAX.push(getData(SHAKEUP_DATA_URL));
 
+$('#transit-details, #transit-open').hide();
+$('.busTips#transit').on('click', function() {
+  $('#transit-details, #transit-open, #transit-closed').toggle();
+});
+
 $(function() {
+
   $.when.apply($, AJAX).done(function() {
     shakeupData = arguments[0].feed.entry;
 
